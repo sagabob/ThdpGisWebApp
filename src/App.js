1818c272310b5571b1054869f32dfa4e36c0c653
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2022-05-14 21:47:16 +1000

    add tailwind css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import MyGlMap from './components/MyGlMap';
 function App() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <MyGlMap />
     </div>
   );
